c12f6214a1a41d8208ccafbc6f3f9cf62fa396ee
Randall Kanna <randallkanna@gmail.com>
2016-01-17 21:59:33 -0800

    initial commit

---
+++ instagramClone/instagramClone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instagramClone
+//
+//  Created by RandallKanna on 1/17/16.
+//  Copyright © 2016 RandallKanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
