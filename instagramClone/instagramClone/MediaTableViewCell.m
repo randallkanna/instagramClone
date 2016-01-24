28239a4e7de928b31ff78b911222c025936dd589
Randall Kanna <randallkanna@gmail.com>
2016-01-23 22:09:01 -0800

    adding fonts for username

--- instagramClone/instagramClone/MediaTableViewCell.m
+++ instagramClone/instagramClone/MediaTableViewCell.m
@@ -19,12 +19,71 @@
 
 @end
 
+static UIFont *lightFont;
+static UIFont *boldFont;
+static UIColor *usernameLabelGray;
+static UIColor *commentLabelGray;
+static UIColor *linkColor;
+static NSParagraphStyle *paragraphStyle;
+
 @implementation MediaTableViewCell
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+        self.mediaImageView = [[UIImageView alloc] init];
+        self.usernameAndCaptionLabel = [[UILabel alloc] init];
+        self.usernameAndCaptionLabel.numberOfLines = 0;
+        self.usernameAndCaptionLabel.backgroundColor = usernameLabelGray;
+        
+        self.commentLabel = [[UILabel alloc] init];
+        self.commentLabel.numberOfLines = 0;
+        self.commentLabel.backgroundColor = commentLabelGray;
+        
+        for (UIView *view in @[self.mediaImageView, self.usernameAndCaptionLabel, self.commentLabel]) {
+            [self.contentView addSubview:view];
+        }
+    }
+    return self;
+}
+
++ (void)load {
+    lightFont = [UIFont fontWithName:@"HelveticaNeue-Thin" size:11];
+    boldFont = [UIFont fontWithName:@"HelveticaNeue-Bold" size:11];
+    usernameLabelGray = [UIColor colorWithRed:0.933 green:0.933 blue:0.933 alpha:1];
+    commentLabelGray = [UIColor colorWithRed:0.898 green:0.898 blue:0.898 alpha:1];
+    linkColor = [UIColor colorWithRed:0.345 green:0.314 blue:0.427 alpha:1];
+    
+    NSMutableParagraphStyle *mutableParagraphStyle = [[NSMutableParagraphStyle alloc] init];
+    mutableParagraphStyle.headIndent = 20.0;
+    mutableParagraphStyle.firstLineHeadIndent = 20.0;
+    mutableParagraphStyle.tailIndent = -20.0;
+    mutableParagraphStyle.paragraphSpacingBefore = 5;
+    paragraphStyle = mutableParagraphStyle;
+}
+
+- (NSAttributedString *) usernameAndCaptionString {
+    CGFloat usernameFontSize = 15;
+    
+    NSString *baseString = [NSString stringWithFormat:@"%@ %@", self.mediaItem.user.userName, self.mediaItem.caption];
+    
+    NSMutableAttributedString *mutableUsernameAndCaptionString = [[NSMutableAttributedString alloc] initWithString:baseString attributes:@{NSFontAttributeName : [lightFont fontWithSize:usernameFontSize], NSParagraphStyleAttributeName : paragraphStyle}];
+    
+    NSRange usernameRange = [baseString rangeOfString:self.mediaItem.user.userName];
+    
+    [mutableUsernameAndCaptionString addAttribute:NSFontAttributeName value:[boldFont fontWithSize:usernameFontSize] range:usernameRange];
+    
+    [mutableUsernameAndCaptionString addAttribute:NSForegroundColorAttributeName value:linkColor range:usernameRange];
+    
+    return mutableUsernameAndCaptionString;
 }
 
 @end
